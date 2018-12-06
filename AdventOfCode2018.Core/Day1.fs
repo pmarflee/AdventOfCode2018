a1e830aabd80c4e232e0856825776db6008b61a8
Paul Marfleet <paul.marfleet@gmail.com>
2018-12-06 21:59:54 +0000

    Added Day 1, Part 2 tests + stub method

--- AdventOfCode2018.Core/Day1.fs
+++ AdventOfCode2018.Core/Day1.fs
@@ -1,4 +1,7 @@
 namespace AdventOfCode2018.Core
 
 module Day1 =
+    open System
+
     let calculatePart1 (input : seq<string>) = Seq.sumBy int input
+    let calculatePart2 (input : seq<string>) = raise <| new NotImplementedException()
--- AdventOfCode2018.Tests/Day1.fs
+++ AdventOfCode2018.Tests/Day1.fs
@@ -7,14 +7,26 @@ namespace Day1
 
         type Tests () =
 
-            static member Data
+            static member Part1Data
                 with get() = [ [| [ "+1"; "-2"; "+3"; "+1" ] :> obj; 3 :> obj |];
                                [| [ "+1"; "+1"; "+1" ] :> obj; 3 :> obj |];
                                [| [ "+1"; "+1"; "-2" ] :> obj; 0 :> obj |];
                                [| [ "-1"; "-2"; "-3" ] :> obj; -6 :> obj |] ]
 
+            static member Part2Data
+                with get() = [ [| [ "+1"; "-2"; "+3"; "+1" ] :> obj; 2 :> obj |];
+                               [| [ "+1"; "-1" ] :> obj; 0 :> obj |] ;
+                               [| [ "+3"; "+3"; "+4"; "-2"; "-4" ] :> obj; 10 :> obj |];
+                               [| [ "-6"; "+3"; "+8"; "+5"; "-6" ] :> obj; 5 :> obj |];
+                               [| [ "+7"; "+7"; "-2"; "-7"; "-4" ] :> obj; 14 :> obj |] ]
+
             [<Theory>]
-            [<MemberData("Data")>]
+            [<MemberData("Part1Data")>]
             member _verify.``Calculate Frequency (Part 1)`` (input: seq<string>, expected: int) =
                 Day1.calculatePart1 input |> should equal expected
+
+            [<Theory>]
+            [<MemberData("Part2Data")>]
+            member _verify.``Calculate First Frequency Reached Twice (Part 2)`` (input: seq<string>, expected: int) =
+                Day1.calculatePart2 input |> should equal expected
                     
