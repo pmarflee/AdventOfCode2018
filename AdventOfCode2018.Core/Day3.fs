b0f8484576554078599e4eea2f4f7c4d53fa6d89
Paul Marfleet <paul.marfleet@gmail.com>
2018-12-08 22:57:35 +0000

    Added source files for Day 3 puzzle Added parser implementation + tests Added Part 1 stub implementation + tests

---
+++ AdventOfCode2018.Core/Day3.fs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2018.Core
+
+module Day3 =
+    open System
+    open System.Text.RegularExpressions
+
+    type Claim = { Number : int;
+                   LeftOffset : int;
+                   TopOffset : int;
+                   Width : int;
+                   Height : int }
+
+    let private _regex = new Regex("^#([0-9]+)\s@\s([0-9]+),([0-9]+):\s([0-9]+)x([0-9]+)$")
+
+    let parse input = 
+        let m = _regex.Match(input)
+        { Number = int m.Groups.[1].Value;
+          LeftOffset = int m.Groups.[2].Value;
+          TopOffset = int m.Groups.[3].Value;
+          Width = int m.Groups.[4].Value;
+          Height = int m.Groups.[5].Value }
+
+    let calculatePart1 (lines : seq<string>) = raise <| new NotImplementedException()
---
+++ AdventOfCode2018.Tests/Day3.fs
@@ -0,0 +1,46 @@
+﻿namespace Day3
+    module Day3 =
+
+        open Xunit
+        open FsUnit.Xunit
+        open AdventOfCode2018.Core.Day3
+        open Xunit.Sdk
+
+        type Tests () =
+
+            static member ParserData
+                with get() = [ [| "#1 @ 1,3: 4x4" :> obj;
+                                  { Number = 1; 
+                                    LeftOffset = 1; 
+                                    TopOffset = 3; 
+                                    Width = 4; 
+                                    Height = 4 } :> obj |];
+                               [| "#2 @ 3,1: 4x4" :> obj;
+                                  { Number = 2;
+                                    LeftOffset = 3;
+                                    TopOffset = 1;
+                                    Width = 4;
+                                    Height = 4 } :> obj |];
+                               [| "#3 @ 5,5: 2x2" :> obj;
+                                  { Number = 3;
+                                    LeftOffset = 5;
+                                    TopOffset = 5;
+                                    Width = 2;
+                                    Height = 2 } |] ]
+
+            static member Part1Data 
+                with get() = [ [| [ "#1 @ 1,3: 4x4"; 
+                                    "#2 @ 3,1: 4x4"; 
+                                    "#3 @ 5,5: 2x2" ] :> obj;
+                                    4 :> obj |] ]
+
+            [<Theory>]
+            [<MemberData("ParserData")>]
+            member _verify.``Parse claim input`` (input : string, expected : Claim) =
+                parse input |> should equal expected
+
+            [<Theory>]
+            [<MemberData("Part1Data")>]
+            member _verify.``Calculate how many square inches of fabric are within 2 or more claims`` (input : seq<string>, expected : int) =
+                calculatePart1 input |> should equal expected
+
