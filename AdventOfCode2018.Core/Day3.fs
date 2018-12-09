eaac3fa9ddf35a295968b4fba968816eb7a5cc3d
Paul Marfleet <paul.marfleet@gmail.com>
2018-12-09 15:46:06 +0000

    Added Day 3 Part 1 solution

--- AdventOfCode2018.Core/Day3.fs
+++ AdventOfCode2018.Core/Day3.fs
@@ -12,12 +12,30 @@ module Day3 =
 
     let private _regex = new Regex("^#([0-9]+)\s@\s([0-9]+),([0-9]+):\s([0-9]+)x([0-9]+)$")
 
-    let parse input = 
-        let m = _regex.Match(input)
+    let parse line = 
+        let m = _regex.Match(line)
         { Number = int m.Groups.[1].Value;
           LeftOffset = int m.Groups.[2].Value;
           TopOffset = int m.Groups.[3].Value;
           Width = int m.Groups.[4].Value;
           Height = int m.Groups.[5].Value }
 
-    let calculatePart1 (lines : seq<string>) = raise <| new NotImplementedException()
+    let calculatePart1 (lines : seq<string>) = 
+        let createMap map line =
+            let claim = parse line
+            let areas = seq {
+                for col = claim.LeftOffset to claim.LeftOffset + claim.Width - 1 do
+                    for row = claim.TopOffset to claim.TopOffset + claim.Height - 1 do
+                        yield (col, row)
+            }
+            let addAreas map' area = 
+                let claims = match Map.tryFind area map' with
+                             | Some(claims') -> Set.add claim claims'
+                             | None -> Set.singleton claim
+                Map.add area claims map'
+            areas |> Seq.fold addAreas map
+        lines 
+        |> Seq.fold createMap Map.empty<(int * int), Set<Claim>>
+        |> Map.toSeq
+        |> Seq.filter (fun (_, claims) -> claims.Count > 1)
+        |> Seq.length
