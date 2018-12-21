07254becc45146965eb40e0813bbc76e74c4efe3
Paul Marfleet <paul.marfleet@gmail.com>
2018-12-21 15:42:17 +0000

    Day 7 - refactor code so that functions can be reused for Part 2 solution

--- AdventOfCode2018.Core/Day7.fs
+++ AdventOfCode2018.Core/Day7.fs
@@ -8,53 +8,54 @@ module Day7 =
 
     let private _lineRegex = new Regex("^Step\s([A-Z])\smust\sbe\sfinished\sbefore\sstep\s([A-Z])\scan\sbegin\.$")
 
-    let calculatePart1 input =
+    let private parse line =
+        let m = _lineRegex.Match(line)
+        (m.Groups.[1].Value.[0], m.Groups.[2].Value.[0])
 
-       let parse line =
-            let m = _lineRegex.Match(line)
-            (m.Groups.[1].Value.[0], m.Groups.[2].Value.[0])
+    let private getStep letter dependent steps =
+        match Map.tryFind letter steps with
+        | Some(step) -> { step with Dependents = step.Dependents.Add dependent }
+        | None -> { Letter = letter; 
+                    Dependents = Set.singleton dependent; 
+                    DependsUpon = Set.empty }
 
-       let getStep letter dependent steps =
-            match Map.tryFind letter steps with
-            | Some(step) -> { step with Dependents = step.Dependents.Add dependent }
-            | None -> { Letter = letter; 
-                        Dependents = Set.singleton dependent; 
-                        DependsUpon = Set.empty }
+    let private getDependent dependent letter steps =
+        match Map.tryFind dependent steps with
+        | Some(step) -> { step with DependsUpon = step.DependsUpon.Add letter }
+        | None -> { Letter = dependent; 
+                    Dependents = Set.empty; 
+                    DependsUpon = Set.singleton letter }
 
-       let getDependent dependent letter steps =
-            match Map.tryFind dependent steps with
-            | Some(step) -> { step with DependsUpon = step.DependsUpon.Add letter }
-            | None -> { Letter = dependent; 
-                        Dependents = Set.empty; 
-                        DependsUpon = Set.singleton letter }
-
-       let createSteps steps line =
+    let private createSteps input = 
+        let createSteps' steps line =
             let (letter, dependent) = parse line
             let step = getStep letter dependent steps
             let dependentStep = getDependent dependent letter steps
             steps |> Map.add letter step |> Map.add dependent dependentStep
-            
-       let steps = Array.fold createSteps Map.empty input
+        
+        Array.fold createSteps' Map.empty input
 
-       let lettersWithNoDependents =
-           steps
-           |> Seq.where (fun kvp -> kvp.Value.DependsUpon.IsEmpty)
-           |> Seq.map (fun kvp -> kvp.Key)
-           |> Set.ofSeq
+    let private getLettersNoDependents (steps : Map<char, Step>) =
+        steps
+        |> Seq.where (fun kvp -> kvp.Value.DependsUpon.IsEmpty)
+        |> Seq.map (fun kvp -> kvp.Key)
+        |> Set.ofSeq
 
-       let find letter = Map.find letter steps
+    let private find steps letter = Map.find letter steps
 
-       let orderGenerator (remaining, (completed : Set<char>)) = 
-           if Set.isEmpty remaining then None
-           else
-               let step =
-                   remaining
-                   |> Seq.map find
-                   |> Seq.where (fun step -> step.DependsUpon |> Set.forall completed.Contains) 
-                   |> Seq.sortBy (fun step -> step.Letter)
-                   |> Seq.head
-               let remaining' = remaining |> Set.remove step.Letter |> Set.union step.Dependents
+    let calculatePart1 input =
+        let steps = createSteps input
+        let generateOrders (remaining, (completed : Set<char>)) = 
+            if Set.isEmpty remaining then None
+            else
+                let step =
+                    remaining
+                    |> Seq.map (find steps)
+                    |> Seq.where (fun step -> step.DependsUpon |> Set.forall completed.Contains) 
+                    |> Seq.sortBy (fun step -> step.Letter)
+                    |> Seq.head
+                let remaining' = remaining |> Set.remove step.Letter |> Set.union step.Dependents
 
-               Some(step.Letter, (remaining', Set.add step.Letter completed))
+                Some(step.Letter, (remaining', Set.add step.Letter completed))
 
-       Seq.unfold orderGenerator (lettersWithNoDependents, Set.empty) |> Array.ofSeq |> String
+        Seq.unfold generateOrders (getLettersNoDependents steps, Set.empty) |> Array.ofSeq |> String
